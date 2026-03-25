0e73fea565d48c3067094d2bf8e45dd7e1ae4241
ruthmarieartistry <71723660+ruthmarieartistry@users.noreply.github.com>
2026-03-24 22:40:24 -0400

    Add .nojekyll fix for GitHub Pages _expo folder

--- fix-paths.js
+++ fix-paths.js
@@ -7,3 +7,7 @@ html = html.replace(/src="\/_expo\//g, 'src="/equestrian-glossary/_expo/');
 html = html.replace(/href="\/_expo\//g, 'href="/equestrian-glossary/_expo/');
 fs.writeFileSync(indexPath, html);
 console.log('Paths fixed in dist/index.html');
+
+// Tell GitHub Pages not to process with Jekyll (so _expo/ folder is served)
+fs.writeFileSync(path.join(__dirname, 'dist', '.nojekyll'), '');
+console.log('.nojekyll created');
